339f17e33a14947390daa4dea07d79cf1efb357d
sonercan <44256962+mhsnabz@users.noreply.github.com>
2024-02-08 15:44:13 +0300

    created project

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by srbrt on 8.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
